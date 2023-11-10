2d2177bb64585f0837603cb065be1b9c861dcd58
liga4 <141454772+liga4@users.noreply.github.com>
2023-11-10 18:34:09 +0200

    uzd

--- app/Router/routes.php
+++ app/Router/routes.php
@@ -4,10 +4,4 @@ return [
     ['GET', '/', ['App\Controllers\ArticleController', 'index']],
     ['GET', '/search', ['App\Controllers\ArticleController', 'search']],
 
-//    ['GET', '/episodes', ['App\Controllers\EpisodeController', 'index']],
-//    ['GET', '/seasons', ['App\Controllers\SeasonController', 'index']],
-//    ['GET', '/season/{id}', ['App\Controllers\SeasonController', 'show']],
-//    ['GET', '/episode/{id}', ['App\Controllers\EpisodeController', 'show']],
-//    ['GET', '/search', ['App\Controllers\EpisodeController', 'search']]
-
 ];
